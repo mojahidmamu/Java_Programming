3c7991b85dfdf92b89c4e27cd84ff1c6371bdf15
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-18 12:23:50 +0600

    added

--- Variable/variables.java
+++ Variable/variables.java
@@ -1,6 +1,11 @@
+
 /**
- * to declare a variable: 
+ * to declare a variable:
  * step-1: data-type
- * step-2: variable name: 
+ * step-2: variable name:
  * step-3: value;
  */
+
+// int age = 10;
+// string name = "Mojahid";
+// double  pi  = 3.1416;
