5804fe996737f57498a00efdcd5ce99342aa4997
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-22 19:20:31 +0600

    added

--- Basics/HelloWorld.java
+++ Basics/HelloWorld.java
@@ -1,4 +1,6 @@
- public class HelloWorld {
+
+public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("Hello World! ");
         System.out.println("Abdullah all Mojahid!\n");
