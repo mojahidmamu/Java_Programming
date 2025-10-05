08758cf2d53aaa59399f495c6d84b07604c7ebca
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-05 16:18:52 +0600

    added

---
+++ Class and Object/StudentExample.java
@@ -0,0 +1,35 @@
+// Define a class
+
+class Student {
+
+    // Fields (Attributes)
+    String color;
+    String brand;
+    int speed;
+
+    // Method (Behavior)
+    void showDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Color: " + color);
+        System.out.println("Speed: " + speed + " km/h");
+    }
+}
+
+// Main class
+public class StudentExample {
+
+    public static void main(String[] args) {
+        // Create object of Car
+        Student s1 = new Student();
+        Student s2 = new Student();
+
+        // Assign values
+        myCar.brand = "Toyota";
+        myCar.color = "Red";
+        myCar.speed = 120;
+
+        // Call method
+        Student.s1();
+        Student.s2();
+    }
+}
