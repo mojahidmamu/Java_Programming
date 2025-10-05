ea13190a0dcab8696502913efb6cba84ed70556d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-05 16:25:53 +0600

    added

--- Class and Object/StudentExample.java
+++ Class and Object/StudentExample.java
@@ -23,18 +23,26 @@ public class StudentExample {
         // Create object of Car
         Student s1 = new Student();
         Student s2 = new Student();
+        Student s3 = new Student();
 
         // Assign values to S1: 
-        s1.name = "Abdullah all Mojahid";
+        s1.name = "Mojahid";
         s1.roll = 884913;
         s1.marks = 32;
 
         // Assign values to S2: 
-        s1.name = "Mahmud";
-        s1.roll = 884888;
-        s1.marks = 85;
+        s2.name = "Mahmud";
+        s2.roll = 884888;
+        s2.marks = 85;
+
+        // Assign values to S3: 
+        s3.name = "Sazzad+Salman";
+        s3.roll = 798716;
+        s3.marks = 96;
+
         // Call method
         s1.showDetails();
         s2.showDetails();
+        s3.showDetails();
     }
 }
