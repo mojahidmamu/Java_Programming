18bc3e48fb1c33ee0e3c9c2b06b251bdca5dc159
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-05 15:19:24 +0600

    added

--- Arrays/arrayExample_2.java
+++ Arrays/arrayExample_2.java
@@ -1,24 +1,17 @@
+
 import java.util.Scanner;
 
 public class arrayExample_2 {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter number of elements: ");
+        System.err.println("Enter the number of elements");
         int n = input.nextInt();
 
         int[] arr = new int[n];
+        // Taking input from user: 
+        System.err.println("Enter " + n + "  elements");
 
-        // Taking input
-        System.out.println("Enter " + n + " numbers:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = input.nextInt();
-        }
-
-        // Printing array
-        System.out.println("You entered:");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        
     }
 }
