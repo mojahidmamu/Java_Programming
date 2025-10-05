63cd0bd125f7da8acee13d6842d14058d4cf0c09
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-05 15:04:34 +0600

    added

--- Operator/operand.java
+++ Operator/operand.java
@@ -1,2 +1,8 @@
-int a = 10, b = 5;
-int sum = a + b;  // '+' is an operator
+// int a = 10, b = 5;
+// int sum = a + b;  // '+' is an operator
+
+
+/**
+ * Topic: Ternary Operator: 
+ * variable = (condition) ? value_if_true : value_if_false;
+ */
