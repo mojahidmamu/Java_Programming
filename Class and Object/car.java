d62d71e6dd6fbf65e64e4bfdca8180aa91a7949d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-05 16:13:35 +0600

    added

---
+++ Class and Object/car.java
@@ -0,0 +1,33 @@
+// Define a class
+
+class Car {
+
+    // Fields (Attributes)
+    String color;
+    String brand;
+    int speed;
+
+    // Method (Behavior)
+    void showDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Color: " + color);
+        System.out.println("Speed: " + speed + " km/h");
+    }
+}
+
+// Main class
+public class car {
+
+    public static void main(String[] args) {
+        // Create object of Car
+        Car myCar = new Car();
+
+        // Assign values
+        myCar.brand = "Toyota";
+        myCar.color = "Red";
+        myCar.speed = 120;
+
+        // Call method
+        myCar.showDetails();
+    }
+}
