4ca10c9754ce910dc110f0d0c931c8b81053db8e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-05 15:07:52 +0600

    added

---
+++ Control_Flow/control.java
@@ -0,0 +1,6 @@
+/**
+ * Type of Control Statement: 
+ * 1. Decision making : if, if-else, else-if, switch, 
+ * 2. Loop : for, while, do-while: 
+ * 3. Jump Statement: break, continue, return..
+ */
