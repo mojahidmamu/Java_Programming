722ed370c3c44e60a1760ea4c870390e497542a4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-05 15:13:22 +0600

    added

--- Arrays/array.java
+++ Arrays/array.java
@@ -1,3 +1,13 @@
 /**
- * 
+ * 1.Declare array: 
+ *      int[] numbers;
+ * 2.Allocate memory; 
+ *      number  = new int[5];
+ * 3.initialize value: 
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+
  */
