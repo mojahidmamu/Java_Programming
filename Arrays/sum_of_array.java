3289503c49e9fd58c40082f40324e7d6e9e78433
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-05 15:31:59 +0600

    added

--- Arrays/sum_of_array.java
+++ Arrays/sum_of_array.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 public class sum_of_array {
 
     public static void main(String[] args) {
-         
+        int[] numbers = {10, 20, 30, 40, 50};
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        System.out.println("Total sum of this array: " + sum);
     }
 }
