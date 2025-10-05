c152e85af026b6c6a3b2c44d5dcf77c46935542c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-05 20:06:07 +0600

    added

--- Class and Object/Constructor/BankExample.java
+++ Class and Object/Constructor/BankExample.java
@@ -10,35 +10,33 @@ class BankAccount {
         balance = amount;
     }
 
-    void diposit(double newAmount) {
+    void deposit(double newAmount) {
         balance += newAmount;
         System.out.println(newAmount + "deposited, New balance : " + balance);
     }
 
-    void withdrow(double Amount) {
-        if(Amount <= balance)
-        {
-            balance -= Amount;
-            System.out.println(Ammount + "withdrawn, Remaining balance : " + balance);
+    void withdraw(double costAmount) {
+        if (costAmount <= balance) {
+            balance -= costAmount;
+            System.out.println(costAmount + "withdrawn, Remaining balance : " + balance);
         } else {
             System.out.println("Insufficient balance !");
         }
     }
 
     void showBalance() {
-        System.out.println(AccountHolder + "'s  balance': " + balance);
+        System.out.println(AccountHolder + " balance: " + balance);
     }
 }
 
 public class BankExample {  // Class name must match file name
     public static void main(String[] args) {
         // --- Your code starts here ---
-        // BankAccount acc1 = new BankAccount("Mojahid", 5000);
         BankAccount client1 = new BankAccount("Mojahid", 5000);
 
-       client1.showBalance();
-       client1.diposit(2500);
-       client1.withdrow(3000);
-       client1.withdrow(8000);
+        client1.showBalance();
+        client1.deposit(2500);
+        client1.withdraw(3000);
+        client1.withdraw(8000);
     }
 }
