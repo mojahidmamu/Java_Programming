6d09bf7d6d5e59b910bd03d717b6990bfa6d1c69
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-06 15:33:33 +0600

    added

---
+++ Inheritance_&_Polymorphism/Polymorphism/Overriding.java
@@ -0,0 +1,9 @@
+class  
+
+
+public class  Overriding{  // Class name must match file name
+    public static void main(String[] args) {
+        // --- Your code starts here ---
+        
+    }
+}
