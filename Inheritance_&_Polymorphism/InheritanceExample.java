ab82f8e98769e114bffda9e61f6d55cb41e13a1c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-06 15:14:40 +0600

    added

--- Inheritance_&_Polymorphism/InheritanceExample.java
+++ Inheritance_&_Polymorphism/InheritanceExample.java
@@ -19,6 +19,8 @@ public class InheritanceExample {  // Class name must match file name
 
     public static void main(String[] args) {
         // --- Your code starts here ---
-
+        Dogs dogs = new Dogs();
+        dogs.eat(); // Inherited from Animal 
+        dogs.barks(); // Defined in Dog
     }
 }
