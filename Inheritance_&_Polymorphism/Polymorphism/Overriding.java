db43ba99869aa8f2a0e6afcfa1e81e4d6f631cb3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-06 15:36:49 +0600

    added

--- Inheritance_&_Polymorphism/Polymorphism/Overriding.java
+++ Inheritance_&_Polymorphism/Polymorphism/Overriding.java
@@ -1,9 +1,32 @@
-class  
 
+class Animal {
 
-public class  Overriding{  // Class name must match file name
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class Overriding {  // Class name must match file name
+
     public static void main(String[] args) {
         // --- Your code starts here ---
-        
+        Animal a;
+        a = new Dog();
+        a.sound();
+
+        a = new Dog();
+        a.sound();
     }
 }
