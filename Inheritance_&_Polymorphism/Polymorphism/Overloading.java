43b61e5709cbc49f6b3206b35942c5548460286a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-06 15:32:03 +0600

    added

--- Inheritance_&_Polymorphism/Polymorphism/Overloading.java
+++ Inheritance_&_Polymorphism/Polymorphism/Overloading.java
@@ -1,6 +1,21 @@
-public class  Overloading{  // Class name must match file name
+
+class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class Overloading {  // Class name must match file name
+
     public static void main(String[] args) {
         // --- Your code starts here ---
-        
+        Calculator calc = new Calculator();
+        System.out.println("Total is: " + calc.add(10, 5));
+        System.out.println("Total is: " + calc.add(10.5, 5.5));
     }
 }
