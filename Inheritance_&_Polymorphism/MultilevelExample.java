5ea8c20f6245f6d30a4aa3bcc36a4ccf4e626425
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-06 15:17:17 +0600

    added

---
+++ Inheritance_&_Polymorphism/MultilevelExample.java
@@ -0,0 +1,20 @@
+class Animal {
+    void eat() { System.out.println("Eating..."); }
+}
+
+class Dog extends Animal {
+    void bark() { System.out.println("Barking..."); }
+}
+
+class Puppy extends Dog {
+    void weep() { System.out.println("Weeping..."); }
+}
+
+public class MultilevelExample {
+    public static void main(String[] args) {
+        Puppy p = new Puppy();
+        p.eat();
+        p.bark();
+        p.weep();
+    }
+}
