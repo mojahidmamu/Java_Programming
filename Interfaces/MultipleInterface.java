fd3fac7ac63767504f0ca938c62c222a26ac875c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-07 15:33:29 +0600

    added

--- Interfaces/MultipleInterface.java
+++ Interfaces/MultipleInterface.java
@@ -1,6 +1,31 @@
-public class  MultipleInterface{  // Class name must match file name
+
+interface A {
+
+    void show();
+}
+
+interface B {
+
+    void display();
+}
+
+class C implements A, B {
+
+    public void show() {
+        System.out.println("Showing from A");
+    }
+
+    public void display() {
+        System.out.println("Displaing from B");
+    }
+}
+
+public class MultipleInterface {  // Class name must match file name
+
     public static void main(String[] args) {
         // --- Your code starts here ---
-        
+        C obj = new C();
+        obj.show();
+        obj.display();
     }
 }
