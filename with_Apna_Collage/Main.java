af2e0ae52369690e555defc616b5fdf3ac2eb0dc
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-08 23:48:44 +0600

    added

--- with_Apna_Collage/Main.java
+++ with_Apna_Collage/Main.java
@@ -1,6 +1,7 @@
-public class Mian {  // Class name must match file name
+public class Main {  // Class name must match file name
     public static void main(String[] args) {
         // --- Your code starts here ---
-        
+        System.out.println("Hello, Apna College!");
     }
 }
+ 
---
+++ with_Apna_Collage/mojahid.java
@@ -0,0 +1,6 @@
+public class Mojahid {
+    public static void main(String[] args) {
+        // --- Your code starts here ---
+        System.out.println("Hello, Mojahid!");
+    }
+}
