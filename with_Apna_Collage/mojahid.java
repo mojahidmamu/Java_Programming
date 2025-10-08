209e5b6f935855ce02973802e1f8144d015d025c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-08 23:52:44 +0600

    added

--- with_Apna_Collage/mojahid.java
+++ with_Apna_Collage/mojahid.java
@@ -1,4 +1,4 @@
-public class Mojahid {
+public class mojahid {
     public static void main(String[] args) {
         // --- Your code starts here ---
         System.out.println("Hello, Mojahid!");
