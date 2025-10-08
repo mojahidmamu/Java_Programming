4eb285fbe904b90ced2dced2a4d9dd9b407599e7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-08 09:07:57 +0600

    added

---
+++ Object-Oriented_Programming_(OOP)/Student_OOP.java
@@ -0,0 +1,27 @@
+
+class Student {
+    String name;
+    int age;
+
+    // Constructor: 
+    Student(String n, int ag) {
+        name = n;
+        age = ag;
+    }
+
+    // method: 
+    void  displayShow()
+    {
+        System.err.println("Name is: " + name + "and Age is: " + age);
+    }
+}
+
+public class Student_OOP {  // Class name must match file name
+
+    public static void main(String[] args) {
+        // --- Your code starts here ---
+        Student s1 = new Student("Mojahid", 18);
+
+        s1.displayShow();
+    }
+}
