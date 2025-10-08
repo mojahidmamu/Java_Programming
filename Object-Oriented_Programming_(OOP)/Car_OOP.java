cbe5cb9d3c998e4214c44a20c70ce1a39dbefcea
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-08 10:19:22 +0600

    added

--- Object-Oriented_Programming_(OOP)/Car_OOP.java
+++ Object-Oriented_Programming_(OOP)/Car_OOP.java
@@ -1,9 +1,35 @@
 
+class Car {
 
+    String brand;
+    int speed;
 
-public class  Car_OOP{  // Class name must match file name
+    // // Contructor: 
+    // Car(String brand, int speed) {
+    //     brand = brand;
+    //     speed = speed;
+    // }
+
+    void start() {
+        System.out.println(brand + " is starting...");
+    }
+
+    void accelerate() {
+        speed += 10;
+        System.out.println("Speed is: " + speed);
+    }
+
+}
+
+public class Car_OOP {  // Class name must match file name
+
     public static void main(String[] args) {
         // --- Your code starts here ---
-        
+        Car c1 =  new  Car();
+        c1.brand = "BMW";
+        c1.speed = 120;
+
+        c1.start();
+        c1.accelerate();
     }
 }
