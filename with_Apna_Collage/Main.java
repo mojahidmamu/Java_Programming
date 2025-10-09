e9b33f18965e2756a9a99c85f183761069cdc808
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-09 12:01:31 +0600

    added

--- with_Apna_Collage/Main.java
+++ with_Apna_Collage/Main.java
@@ -111,5 +111,27 @@ public class Main {  // Class name must match file name
         System.out.println("p && q: " + (p && q));
         System.out.println("p || q: " + (p || q));
         System.out.println("!p: " + (!p));
+
+        // switch case
+        int day = 3;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
     }
 }
