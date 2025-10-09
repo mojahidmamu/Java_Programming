e51ce61112f722b2071fb8a5eaf832de77448e8d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-09 13:32:31 +0600

    added

--- with_Apna_Collage/MiniProject.java
+++ with_Apna_Collage/MiniProject.java
@@ -1,10 +1,26 @@
-
+import java.util.Scanner;
 public class MiniProject {
 
     public static void main(String[] args) {
         // --- Your code starts here ---  
-
         // MINI PROJECT: Simple Banking System 
-        int myNumer = (int)(Math.random()*100);
+        
+        Scanner sc = new Scanner(System.in);
+        int myNumer = (int) (Math.random() * 100);
+        do {
+            System.err.println("Guess the number");
+            int userNumber = sc.nextInt();
+
+            if (userNumber == myNumer) {
+                System.out.println("You guessed it right");
+                break;
+            } 
+            else if (userNumber < myNumer) {
+                System.out.println("Too low");
+            }
+             else {
+                System.out.println("Too high");
+            }
+        }  
     }
 }
