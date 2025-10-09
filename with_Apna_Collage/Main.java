9a9523e0ea2478dcb75be9f3fa1af5192ec3d6e1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-09 11:27:02 +0600

    added

--- with_Apna_Collage/Main.java
+++ with_Apna_Collage/Main.java
@@ -1,13 +1,16 @@
+
 import java.lang.reflect.Array;
 import java.util.Arrays;
+
 public class Main {  // Class name must match file name
+
     public static void main(String[] args) {
         // --- Your code starts here ---
         System.out.println("Hello, Apna College!");
 
         System.out.println("This is my first Java program.");
         System.out.println("I am excited to learn Java programming.");
-        System.out.println("Java is a versatile and powerful language.");   
+        System.out.println("Java is a versatile and powerful language.");
 
         int[] marks = new int[5];
         marks[0] = 85;
@@ -23,6 +26,19 @@ public class Main {  // Class name must match file name
         // sorting the array
         Arrays.sort(marks);
         System.out.println("Sorted marks: " + Arrays.toString(marks));
+
+        // 2D Array
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        System.out.println("2D Matrix:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
- 
