46fac9b66c1b487e1858380a22ca45b46b07d591
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-09 17:02:48 +0600

    added

---
+++ Mini Project/MiniProject.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class MiniProject {
+    public static void main(String[] args) {
+        // --- Your code starts here ---  
+        
+        // MINI PROJECT: Simple Banking System 
+        Scanner sc = new Scanner(System.in);
+
+        // Randdom number between 0-99: 
+        int myNumer = (int) (Math.random() * 100);
+        int userNumber = 0;
+        do {
+            System.out.println("Guess the number: 1-100");
+            userNumber = sc.nextInt();
+
+            if (userNumber == myNumer) {
+                System.out.println("You guessed  it right");
+                break;
+            }
+            else if (userNumber < myNumer) {
+                System.out.println("Too low, try again.");
+            }
+             else {
+                System.out.println("Too high, try again.");
+            }
+        } while (userNumber != myNumer);
+
+        System.out.println("The correct number was: " + myNumer);
+        sc.close(); 
+    }   
+}
