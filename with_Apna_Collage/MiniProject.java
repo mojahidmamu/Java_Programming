5b8521aa67e19cee6a7f1fc078b533ccc26791e6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-09 13:27:32 +0600

    added

--- with_Apna_Collage/MiniProject.java
+++ with_Apna_Collage/MiniProject.java
@@ -4,5 +4,7 @@ public class MiniProject {
     public static void main(String[] args) {
         // --- Your code starts here ---  
 
+        // MINI PROJECT: Simple Banking System 
+        int myNumer = (int)(Math.random()*100);
     }
 }
