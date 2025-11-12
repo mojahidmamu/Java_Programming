0bc2b22d9fe36e53461fa9c8a36b027b9b8001b5
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-12 10:57:40 +0600

    added

--- Practice_Problem/first.java
+++ Practice_Problem/first.java
@@ -103,10 +103,10 @@ public class first {
         // Fibonacci series
         int terms = 10;
         int first = 0, second = 1;
-        System.out.print("Fibonacci Series: " + first + " " + second + " ");
+        System.out.print("Fibonacci Series: " + first + ", " + second);
         for (int i = 3; i <= terms; i++) {
             int next = first + second;
-            System.out.print(next + " ");
+            System.out.print(", " + next);
             first = second;
             second = next;
         }
