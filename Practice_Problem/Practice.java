0059842a433902a3533a392625066e7f62a45683
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-12 11:08:32 +0600

    added

--- Practice_Problem/Practice.java
+++ Practice_Problem/Practice.java
@@ -1,11 +1,13 @@
 
 public class Practice {
     public static void main(String[] args) {
+        // sum of three numbers
         int a = 5;
         int b = 10;
         int c = 20;
         int sum = a + b + c;
         System.out.println("The sum of: " + sum);
+
         // Area of rectangle
         int height = 10;
         int width = 20;
