b02488d1385c0d6ed4a48dde5f9fbf82cf510d59
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-12 10:46:53 +0600

    added

--- Practice_Problem/first.java
+++ Practice_Problem/first.java
@@ -70,5 +70,24 @@ public class first {
         } else {
             System.out.println("Roots are not possible");
         }
+
+        // prime number check
+        int number = 29;
+        boolean isPrime = true;
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
     }
 }
