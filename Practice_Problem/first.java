5f7056254310a7e3d1b58689342f220e2dc22cea
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-12 09:41:01 +0600

    added

--- Practice_Problem/first.java
+++ Practice_Problem/first.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 
 public class first {
     public static void main(String[] args) {
-        // int a = 5;
-        // int b = 10;
-        // int c = 20;
-        // int sum = a + b + c;
-        // System.out.println("The sum of: " + sum);
+        int a = 5;
+        int b = 10;
+        int c = 20;
+        int sum = a + b + c;
+        System.out.println("The sum of: " + sum);
+        // Area of rectangle
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int area = a * b;
+        int height = sc.nextInt();
+        int width = sc.nextInt();
+        int area = height * width;
         System.out.println("The area of rectangle is: " + area);
     }
 }
