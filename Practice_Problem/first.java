f0a540f9612fea9f9b9143f46658b33ece0590e6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-12 09:34:45 +0600

    added

--- Practice_Problem/first.java
+++ Practice_Problem/first.java
@@ -1,5 +1,9 @@
 public class first {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int a = 5;
+        int b = 10;
+        int c = 20;
+        int sum = a + b + c;
+        System.out.println("The sum of: " + sum);
     }
 }
