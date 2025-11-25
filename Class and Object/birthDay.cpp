a57da62939c04fde7128ce48fc550257e4339934
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-25 21:08:10 +0600

    added

---
+++ Class and Object/birthDay.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string name = "Abdullaha all Mojahid";
+
+    cout << "========================================\n";
+    cout << "         Happy Birthday Program \n";
+    cout << "========================================\n\n";
+
+    cout << "Dear " << name << ",\n\n";
+    cout << "Happy Birthday! \n";
+    cout << "Wishing you a year full of success, joy,\n";
+    cout << "and amazing opportunities ahead.\n\n";
+    cout << "May your dreams get closer,\n";
+    cout << "your hard work shine brighter,\n";
+    cout << "and your smile stay forever.\n\n";
+
+    cout << "Keep believing in yourself and\n";
+    cout << "never stop moving forward. \n\n";
+
+    cout << "Best wishes,\n";
+    cout << "Your friend \n";
+
+    cout << "\n========================================\n";
+    return 0;
+}
