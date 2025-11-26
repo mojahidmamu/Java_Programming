34138d69897486b52ec2d29626c510dc86f24f5a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-26 12:38:31 +0600

    added

---
+++ Class and Object/Method.java
@@ -0,0 +1,23 @@
+
+class Student {
+
+    String name;
+    int age;
+
+    void showDetails() {
+        System.out.println("Name is: " + name + "and age is: " + age);
+    }
+}
+
+public  Method {  // Class name must match file name
+
+    public static void main(String[] args) {
+        // --- Your code starts here ---
+        Student s1 = new Student();
+        s1.name = "Mojahid";
+        s1.age = 18;
+
+        // method call: 
+        s1.showDetails();
+    }
+}
