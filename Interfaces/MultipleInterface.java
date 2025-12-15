dbe436767ab09d160415d84b7045cc05c901b691
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-15 11:59:10 +0600

    added

--- Interfaces/MultipleInterface.java
+++ Interfaces/MultipleInterface.java
@@ -1,16 +1,13 @@
 
 interface A {
-
     void show();
 }
 
 interface B {
-
     void display();
 }
 
 class C implements A, B {
-
     public void show() {
         System.out.println("Showing from A");
     }
