46996700c81d13dd68dbc2cec5dcbeafff4fe335
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-23 09:43:51 +0600

    added

--- Interfaces/MultipleInterface.java
+++ Interfaces/MultipleInterface.java
@@ -1,13 +1,16 @@
 
 interface A {
+
     void show();
 }
 
 interface B {
+
     void display();
 }
 
 class C implements A, B {
+
     public void show() {
         System.out.println("Showing from A");
     }
--- Package/PackageExample.java
+++ Package/PackageExample.java
@@ -1,5 +1,5 @@
 
-import java.util.Scanner;
+import java.util.Scanner; // package
 
 public class PackageExample {
 
