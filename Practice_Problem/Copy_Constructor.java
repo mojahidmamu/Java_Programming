d15d7bc4c451b680400f05bc9a92c232a4b58a9a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-23 10:35:51 +0600

    added

--- Practice_Problem/Copy_Constructor.java
+++ Practice_Problem/Copy_Constructor.java
@@ -4,22 +4,16 @@ class Student {
     String name;
     int id;
 
-    // copy constructor-1: (No parameter) 
-    Student() {
-        name = "Mojahid";
-        id = 0;
-    }
-
-    // copy constructor-2: (one parameter) 
-    Student(int i) {
-        name = "Mojahid";
-        id = i;
-    }
-
-    // copy constructor-3: (two parameter) 
-    Student(String nm, int i) {
+    // normal constructor
+    Student(String nm , int i) {
         name = nm;
         id = i;
+    }
+
+    // copy constructor 
+    Student(Student s) {
+        name = s.name;
+        id = s.id;
     }
 
     void  display() {
@@ -31,12 +25,12 @@ class Student {
 public class Copy_Constructor {
 
     public static void main(String[] args) {
-        Student s1 = new  Student();
-        Student s2 = new  Student(10);
-        Student s3 = new  Student("abdullah", 11);
+        Student s1 = new  Student("Mojahid", 11);
+        Student s2 = new  Student(s1);
+
 
         s1.display();
         s2.display();
-        s3.display();
+       
     }
 }
