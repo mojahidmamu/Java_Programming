a98bee1b22f155031d8d8676e999acd894134a9a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-12-23 09:52:32 +0600

    added

---
+++ Practice_Problem/Method_Overloading.java
@@ -0,0 +1,25 @@
+class MathOperation {
+    int add(int a, int b) {
+        return  a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return  a + b + c;
+    }
+
+    double add(double  a, double b) {
+        return  a + b;
+    }
+}
+
+
+public class Method_Overloading {   
+
+    public static void main(String[] args) {
+        // --- Your code starts here ---
+        MathOperation obj = new MathOperation();
+        System.out.println(obj.add(10, 20));
+        System.out.println(obj.add(10, 20, 30));
+        System.out.println(obj.add(10.5, 20.8));
+    }
+}
