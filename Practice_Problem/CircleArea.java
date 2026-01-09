3ee9a3a9b335b03679bb95b02b15aa1b91bb341a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-09 19:18:46 +0600

    mojahidMamu

--- Practice_Problem/CircleArea.java
+++ Practice_Problem/CircleArea.java
@@ -1,8 +1,30 @@
 
+class over {
+
+    void area(int a, int b) {
+        int z = a + b;
+        System.out.println("Area of " + z);
+        return;
+    }
+
+    void area(double a, double b, double c) {
+        double z = a * b * c;
+        System.out.println("Area of " + z);
+        return;
+    }
+
+     void area(int sintia) {
+        System.out.println("Girlfriend of Rezuyan: " + sintia);
+        return;
+    }
+
+
+}
+
 public class CircleArea {
 
     public static void main(String args[]) {
-        // Prime : 1-100 with for loop: 
-         
+        //  method overloading in Java
+
     }
 }
