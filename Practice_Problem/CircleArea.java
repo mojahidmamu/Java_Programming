abf59594f3964455eef65261322d62b0a11aaeb5
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-09 18:53:54 +0600

    mojahidMamu

--- Practice_Problem/CircleArea.java
+++ Practice_Problem/CircleArea.java
@@ -6,18 +6,18 @@ public class CircleArea {
         double a = 1;
         double b = -5;
         double c = 6;
-        double discriminant = b * b - 4 * a * c;
-        if (discriminant > 0) {
-            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-            System.out.println("Roots are real and different.");
-            System.out.println("Root 1: " + root1);
-            System.out.println("Root 2: " + root2);
-        } else if (discriminant == 0) {
+        double d = (b * b) - (4 * a * c);
+        if (d > 0) {
+            double root1 = (-b + Math.sqrt(d)) / (2 * a);
+            double root2 = (-b - Math.sqrt(d)) / (2 * a);
+            System.out.println("Root 1: " + root1 + " Root 2: " + root2);
+        } else if (d == 0) {
             double root = -b / (2 * a);
-            System.out.println("Roots are real and the same.");
             System.out.println("Root: " + root);
         } else {
-            System.out.println("Roots are complex and different.");
+            System.out.println("Roots are impossible.");
+
         }
 
+    }
+}
