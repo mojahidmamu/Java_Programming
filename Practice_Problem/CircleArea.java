a104b03f5f1310e65f5eb79e311030c08da3afbe
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-09 19:09:06 +0600

    mojahidMamu

--- Practice_Problem/CircleArea.java
+++ Practice_Problem/CircleArea.java
@@ -3,18 +3,6 @@ public class CircleArea {
 
     public static void main(String args[]) {
         // Prime : 1-100 with for loop: 
-        for (int i = 2; i <= 100; i++) {
-            boolean isPrime = true;
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime) {
-                System.out.print(i + " ");
-            }
-        }
-
+         
     }
 }
