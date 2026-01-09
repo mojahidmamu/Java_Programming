6e5f73b543406456d4a8b1412f20306a70adf047
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-09 18:39:56 +0600

    mojahidMamu

--- Practice_Problem/CircleArea.java
+++ Practice_Problem/CircleArea.java
@@ -1,13 +1,18 @@
 
-import java.util.Scanner;
-
 public class CircleArea {
 
     public static void main(String args[]) {
 
-        Scanner iktiar = new Scanner(System.in);
-        int radius = iktiar.nextInt();
-        double Total = Math.PI * radius * radius;
-        System.out.println("Total : " + Total);
+        // Scanner iktiar = new Scanner(System.in);
+        int a = 18;
+        int b = 20;
+        int c = 30;
+        if ((a + b) > c && (a + c) > b && (b + c) > a) {
+            int s = (a + b + c) / 2;
+            double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+            System.out.println("Area of triangle is: " + area);
+        } else {
+            System.out.println("No, triangle is not possible");
+        }
     }
 }
