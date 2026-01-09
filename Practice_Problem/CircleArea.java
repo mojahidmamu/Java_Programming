3c74b6a43b73bc4ad54eb912ae5e63412f0f15a0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-09 18:55:51 +0600

    mojahidMamu

--- Practice_Problem/CircleArea.java
+++ Practice_Problem/CircleArea.java
@@ -2,21 +2,18 @@
 public class CircleArea {
 
     public static void main(String args[]) {
-        // Quadratic Equation: ax^2 + bx + c = 0
-        double a = 1;
-        double b = -5;
-        double c = 6;
-        double d = (b * b) - (4 * a * c);
-        if (d > 0) {
-            double root1 = (-b + Math.sqrt(d)) / (2 * a);
-            double root2 = (-b - Math.sqrt(d)) / (2 * a);
-            System.out.println("Root 1: " + root1 + " Root 2: " + root2);
-        } else if (d == 0) {
-            double root = -b / (2 * a);
-            System.out.println("Root: " + root);
-        } else {
-            System.out.println("Roots are impossible.");
-
+        // Prime : 1-100 with for loop: 
+        for (int i = 2; i <= 100; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
         }
 
     }
