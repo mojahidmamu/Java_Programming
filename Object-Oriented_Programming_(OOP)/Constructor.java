2911ae8e542fa1cfdb058314c3fdd769193c8bf1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-04-08 14:30:19 +0600

    added

---
+++ Object-Oriented_Programming_(OOP)/Constructor.java
@@ -0,0 +1,27 @@
+class Student {
+    String name;
+    int age;
+
+    // Constructor
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method to display student information
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class Constructor {
+     ublic static void main(String[] args) {
+
+        Student s1 = new Student("Mojahid" , 120 );
+        Student s2 = new Student("Abdullah" , 200 );
+        s1.displayInfo();
+        s2.displayInfo();
+
+    }
+}
